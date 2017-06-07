a084dd133ccb0e8af17b9e1f268cccc55690fa71
VictorMs <isvictorma@gmail.com>
2017-06-06 20:15:30 -0700

    Summay
    first time commit

---
+++ symobj1/README.md
@@ -0,0 +1,4 @@
+symobj1
+=======
+
+A Symfony project created on June 6, 2017, 9:25 pm.
---
+++ symobj1/src/AppBundle/Controller/MoveController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Victor
+ * Date: 6/6/2017
+ * Time: 4:47 PM
+ */
+
+namespace AppBundle\Controller;
+
+
+class MoveController
+{
+
+}
